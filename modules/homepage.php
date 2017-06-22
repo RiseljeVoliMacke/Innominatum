111bd2d93c2b37dffe0a4930aafeee95c400de43
TheReaLedu <dmilenkovic@ymail.com>
2017-06-22 12:25:53 +0200

    1.1

---
+++ modules/homepage.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <title>Home Page</title>
+    <meta charset="utf8">
+	<link rel="stylesheet" href="../css/homepage.css">
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+	<div class="container">
+		<h1>Page</h1>
+		<p>This is some text.</p> 
+	</div>
+	
+</body>
+</html>
